00531cdc277f67d254d6fbc9f2bbc3e2060a3b4f
Fernando Torres <fernandotorrescandeia@gmail.com>
2024-07-31 00:14:31 -0300

    adding svg's

--- src/components/ExperienciasCard.js
+++ src/components/ExperienciasCard.js
@@ -1,7 +1,12 @@
 export default function ExperienciasCard() {
   return (
     <div className="bg-base-100 flex flex-col items-center justify-center w-11/12 mx-auto rounded-3xl my-7">
-      <h1 className="text-2xl py-3">Experiencias</h1>
+      <div className="flex flex-row justify-around gap-48 w-full">
+        <h1 className="text-2xl py-3">Experiencias</h1>
+        <button className="bg-slate-400 text-3xl text-black text-center duration-400 hover:bg-slate-300 transition-colors hover:cursor-pointer pb-1 w-10 h-10 mt-2 rounded-full">
+          +
+        </button>
+      </div>
       <div className="card bg-base-300 w-11/12 mb-3">
         <div className="card-body p-4">
           <h2 className="card-title">Shoes!</h2>
--- src/components/HabilidadesCard.js
+++ src/components/HabilidadesCard.js
@@ -1,11 +1,16 @@
 export default function HabilidadesCard() {
   return (
     <div className="bg-base-100 flex flex-col items-start justify-between px-5 w-11/12 mx-auto rounded-3xl my-7 h-28">
-      <h1 className="text-2xl py-3">Habilidades</h1>
-      <div className="card-actions justify-end pb-4">
-        <div className="badge badge-secondary">NEW</div>
-        <div className="badge badge-outline">Fashion</div>
-        <div className="badge badge-outline">Products</div>
+      <div className="flex flex-row justify-between w-full">
+        <h1 className="text-2xl py-3">Habilidades</h1>
+        <button className="bg-slate-400 text-3xl text-black text-center duration-400 hover:bg-slate-300 transition-colors hover:cursor-pointer mr-8 md:mr-0 pb-1 w-10 h-10 mt-2 rounded-full">
+          +
+        </button>
+      </div>
+      <div className="w-full card-actions justify-end pb-4">
+        <div className="badge badge-secondary h-6">NEW</div>
+        <div className="badge badge-outline h-6">Fashion</div>
+        <div className="badge badge-outline h-6">Products</div>
       </div>
     </div>
   );

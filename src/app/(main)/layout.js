e71e67407349c996978299f13309c4339b50f18e
YoYolops <yohanhxh@gmail.com>
2024-08-17 14:39:45 -0300

    Adiciona dependencia ky

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,6 +1,9 @@
+"use client"
 import { useContext, useEffect } from "react";
 import SessionContext from "@/contexts/sessionContext";
 import { redirect } from "next/navigation";
+import Navbar from "@/components/Navbar";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
 export default function MainAppLayout({ children }) {
     const session = useContext(SessionContext);
@@ -11,6 +14,7 @@ export default function MainAppLayout({ children }) {
         if(session.isError || !session.data) redirect("/login");
     }, [session.isLoading])
 
+    if(session.isLoading) return <LoadingSpinner />
     return (
         <>
             <main className="flex justify-center w-full items-center mt-[64px]">

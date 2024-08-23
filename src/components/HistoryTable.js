5189a77887425cd426ae56afd9310629baeb5e13
YoYolops <yohanhxh@gmail.com>
2024-08-23 20:31:09 -0300

    Refatora: histiry page

--- src/components/HistoryTable.js
+++ src/components/HistoryTable.js
@@ -1,54 +1,55 @@
 import { useState } from "react";
 import HistoryTableRow from "./HistoryTableRow";
+import NoDataFound from "./NoDataFound";
 
 const discs = [
   {
-    id: "123456",
+    id: "123456s",
     name: "Teoria Dos Grafos",
     score: "8,5",
   },
   {
-    id: "1234567",
+    id: "1234567a",
     name: "Teoria Dos Grafos",
     score: "8,5",
   },
   {
-    id: "123456",
+    id: "123456c",
     name: "Teoria Dos Grafos",
     score: "8,5",
   },
   {
-    id: "1234567",
+    id: "1234567f",
     name: "Teoria Dos Grafos",
     score: "8,5",
   },
   {
-    id: "123456",
+    id: "123456yy",
     name: "Teoria Dos Grafos",
     score: "8,5",
   },
   {
-    id: "1234567",
+    id: "1234567het",
     name: "Teoria Dos Grafos",
     score: "8,5",
   },
   {
-    id: "123456",
+    id: "123456hetettheh6t",
     name: "Teoria Dos Grafos",
     score: "8,5",
   },
   {
-    id: "1234567",
+    id: "1234567sytker8k",
     name: "Teoria Dos Grafos",
     score: "8,5",
   },
   {
-    id: "123456",
+    id: "123456abtqw",
     name: "Teoria Dos Grafos",
     score: "8,5",
   },
   {
-    id: "1234567",
+    id: "1234567ww",
     name: "Teoria Dos Grafos",
     score: "8,5",
   },
@@ -57,51 +58,26 @@ const discs = [
 export default function HistoryTable() {
   const [history, setHistory] = useState(discs);
 
-  if (history.length === 0) {
-    return (
-      <div className="my-10 flex flex-row gap-3 items-center justify-center w-full">
-        <button className="btn btn-square btn-outline">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
-        <p>Nenhum dado encontrado</p>
-      </div>
-    );
-  }
-
+  if (history.length === 0) return <NoDataFound />
   return (
-    <div className="overflow-x-auto w-11/12 bottom-shadow">
-      <table className="table table-xs bg-base-100">
+    <table className="table custom_shadow max-w-[1000px]">
         <thead>
-          <tr>
+            <tr>
             <th className="text-white font-normal">Cód</th>
             <th className="text-white font-normal">Nome</th>
             <th className="text-white font-normal">Nota</th>
-          </tr>
+            </tr>
         </thead>
         <tbody>
-          {discs.map((disc) => (
-            <HistoryTableRow
-              key={disc.id}
-              id={disc.id}
-              name={disc.name}
-              score={disc.score}
-            />
-          ))}
+            {discs.map((disc) => (
+                <HistoryTableRow
+                    key={disc.id}
+                    id={disc.id}
+                    name={disc.name}
+                    score={disc.score}
+                />
+            ))}
         </tbody>
-      </table>
-    </div>
+    </table>
   );
 }

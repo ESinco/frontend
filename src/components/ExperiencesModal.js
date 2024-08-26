0569c3ac5b1572f3dcc48ab5f90f9cbfeaf35464
Fernando Torres <fernandotorrescandeia@gmail.com>
2024-08-26 15:07:02 -0300

    refatorando modal

--- src/components/ExperiencesModal.js
+++ src/components/ExperiencesModal.js
@@ -11,8 +11,8 @@ export default function ModalSkills() {
         <div className="modal-box p-5 flex flex-col items-center justify-between">
           <h3 className=" text-lg text-center">Adicionar Links Úteis</h3>
 
-          <div className="modal-action">
-            <form method="dialog" className="flex flex-col gap-10 items-end">
+          <div className="modal-action w-full justify-normal">
+            <form method="dialog" className="flex flex-col gap-3  pt-4 w-full ">
               <label className="input input-bordered flex items-center gap-2">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -23,11 +23,35 @@ export default function ModalSkills() {
                   <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
                   <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
                 </svg>
-                <input type="text" className="grow" placeholder="" />
+                <input type="text" className="grow" placeholder="Linkedin" />
+              </label>
+              <label className="input input-bordered flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70"
+                >
+                  <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                  <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                </svg>
+                <input type="text" className="grow" placeholder="Linkedin" />
+              </label>
+              <label className="input input-bordered flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70"
+                >
+                  <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                  <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                </svg>
+                <input type="text" className="grow" placeholder="Linkedin" />
               </label>
 
               {/* if there is a button in form, it will close the modal */}
-              <div className="gap-4 flex">
+              <div className="gap-4 flex pt-10 ml-auto">
                 <button className="btn btn-error ">Cancelar</button>
                 <button className="btn btn-success ">Confirmar</button>
               </div>

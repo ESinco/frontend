04c6cd0bb41c86fe933516f9f442c2381fa24845
Levi Queiroz <leviqueroz@hotmail.com>
2024-09-07 13:35:45 -0300

    ajustando NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function NavBar() {
+export default function Navbar() {
     return (
         <div className="navbar bg-base-100 flex justify-between">
             <div className="">

8e0c2d4f7cf40e676f3eb5f438ece0380d6932fc
Fernando Torres <fernandotorrescandeia@gmail.com>
2024-09-22 15:48:15 -0300

    ök

--- src/app/(main)/professor/details/student/[studentId]/page.js
+++ src/app/(main)/professor/details/student/[studentId]/page.js
@@ -37,7 +37,7 @@ export default function StudentDetails() {
   }
 
   return (
-    <div className="bg-base-200 flex justify-center flex-col items-center p-3 max-w-[1000px]">
+    <div className="bg-base-200 flex justify-start flex-col items-center p-3 w-full h-screen">
       {/* DIV INFORMACOES INICIAIS */}
       <div className="flex justify-center flex-col w-full items-start mb-6">
         <div className=" flex flex-row items-center w-full">

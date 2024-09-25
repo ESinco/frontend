a2c13bf4819531a0d1e92953ef748f034d893162
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2024-09-25 08:25:22 -0300

    feat: adicionado botão para importar projetos pelo CSV

--- src/app/(main)/professor/page.js
+++ src/app/(main)/professor/page.js
@@ -2,6 +2,7 @@
 import { useQuery } from "@tanstack/react-query";
 import ProfessorProjectCard from "@/components/ProfessorProjectCard";
 import CreateProject from "@/components/modals/CreateProject";
+import ImportProject from "@/components/modals/ImportProject";
 import EditProject from "@/components/modals/EditProject";
 import { useContext, useState,useEffect } from "react";
 import SessionContext from "@/contexts/sessionContext";
@@ -33,13 +34,21 @@ export default function Projects() {
                         e.stopPropagation()
                         CreateProject.open()
                     }}
-                >Adicionar +</button>
+                >Criar Projeto</button>
+                <button
+                    className="btn btn-primary btn-xs"
+                    onClick={e => {
+                        e.stopPropagation()
+                        ImportProject.open()
+                    }}
+                >Importar Projeto</button>
             </section>
             <section className="flex flex-col justify-start items-center gap-5 w-full max_width p-3">
                 { projects.isLoading && !!projects.data }
                 { projects.data?.map(project => <ProfessorProjectCard key={project.id} {...project} requestEdit={requestEdit} />) }
             </section>
             <CreateProject.Modal />
+            <ImportProject.Modal />
             <EditProject.Modal editData={editData} />
         </main>
     )
